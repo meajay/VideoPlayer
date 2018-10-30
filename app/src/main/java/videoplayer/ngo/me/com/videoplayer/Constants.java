d089805e7aeafdf369b25d55045abec965b841a1
ajay.chauhan1 <ajay.chauhan@gcf.education>
2018-10-30 22:23:01 +0530

    exo player integrated

---
+++ app/src/main/java/videoplayer/ngo/me/com/videoplayer/Constants.java
@@ -0,0 +1,8 @@
+package videoplayer.ngo.me.com.videoplayer;
+
+/**
+ * Created by Ajay on 30-10-2018.
+ */
+public class Constants {
+    public final static String VIDEO_URL = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4" ;
+}
--- app/src/main/java/videoplayer/ngo/me/com/videoplayer/VideoActivity.java
+++ app/src/main/java/videoplayer/ngo/me/com/videoplayer/VideoActivity.java
@@ -1,15 +1,114 @@
 package videoplayer.ngo.me.com.videoplayer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class VideoActivity extends AppCompatActivity {
+
+    @BindView(R.id.video_view)
+    SimpleExoPlayerView exoPlayerView;
+
+
+    private SimpleExoPlayer exoPlayer;
+    private boolean playWhenReady = false;
+    private long playbackPosition = 0;
+    private int currentWindow = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT > 23 || exoPlayer==null) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        hideSystemUi();
+        if ((Util.SDK_INT <= 23 || exoPlayer == null)) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT > 23) {
+            releasePlayer();
+        }
+    }
+
+    private void initializePlayer() {
+        exoPlayer = ExoPlayerFactory.newSimpleInstance(
+                new DefaultRenderersFactory(this),
+                new DefaultTrackSelector(), new DefaultLoadControl());
+
+        exoPlayerView.setPlayer(exoPlayer);
+        exoPlayer.setPlayWhenReady(playWhenReady);
+        exoPlayer.seekTo(currentWindow, playbackPosition);
+
+        Uri uri = Uri.parse(Constants.VIDEO_URL);
+        MediaSource mediaSource = buildMediaSource(uri);
+        exoPlayer.prepare(mediaSource, true, false);
 
 
+    }
+
+    private void releasePlayer() {
+        if (exoPlayer != null) {
+            playbackPosition = exoPlayer.getCurrentPosition();
+            currentWindow = exoPlayer.getCurrentWindowIndex();
+            playWhenReady = exoPlayer.getPlayWhenReady();
+            exoPlayer.release();
+            exoPlayer = null;
+        }
+    }
+
+    private void hideSystemUi() {
+        exoPlayerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+    private MediaSource buildMediaSource(Uri uri) {
+        return new ExtractorMediaSource.Factory(
+                new DefaultHttpDataSourceFactory("video-player")).
+                createMediaSource(uri);
     }
 }
